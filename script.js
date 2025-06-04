7c1774bd57ae0134c6616d452e278b87caa8dbb0
mikenstudio <214396880+mikenstudio@users.noreply.github.com>
2025-06-04 18:21:53 -0400

    Begin script logic, senses input

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const display = document.querySelector(".display");
+const buttonGroup = document.querySelectorAll(".button");
+const numButtons = buttonGroup.length;
 
-console.log(display);
+console.log(buttonGroup);
+console.log(buttonGroup.length);
+console.log(buttonGroup[1].textContent);
+
+//textContent
+
+for (let i = 0; i < numButtons; i++) {
+  const currentButton = buttonGroup[i];
+  const buttonValue = currentButton.textContent;
+  currentButton.addEventListener("click", (e) => {
+    display.textContent = `${buttonValue}`;
+  });
+}
